3c9ebe8604c606045534b44148f047e676463131
Anselmo <ans.adv2012@gmail.com>
2021-01-14 21:55:26 +0000

    projeto criado

---
+++ supermercado/supermercado/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace supermercado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
